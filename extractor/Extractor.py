88eaa2cb6ab2f07edb0c8a9a34a928a9ab4c329c
Richard Hadden <oculardexterity@gmail.com>
2015-07-21 17:45:21 +0100

    page duplicates working

--- extractor/Extractor.py
+++ extractor/Extractor.py
@@ -1,10 +1,20 @@
+import os
+import shelve
 import xlrd
 
-class Extractor:
+class Stream:
 	def __init__(self, f):
-		self.sheet = xlrd.open_workbook(f).sheet_by_index(0)
-		self.headers = self.getHeaders()
-		self.data = {}
+		# Dependent on file type:
+		if f.endswith('xlsx'):
+			self.sheet = xlrd.open_workbook(f).sheet_by_index(0)
+			self.headers = self.getHeaders()
+			self.stream = self.outputRowsFromFile
+		elif f.endswith('shelve'):
+			self.shelf = shelve.open(f)
+			self.stream = outputRowsFromShelf
+		else:
+			raise ValueError("File must be an Excel spreadsheet or a Shelve file")
+
 
 	def thing(self): # A sanity test
 		return 'test'
@@ -18,8 +28,72 @@ class Extractor:
 						for col_index in range(self.sheet.ncols)]
 						for i in range(1,self.sheet.nrows)]
 
-	def buildRowDict(self, row):
+	def buildRowTuple(self, row):
 		id_index = self.headers.index(self.id_column)
-		return {str(row[id_index]) : {self.headers[i]: cell for i, cell in enumerate(row)}}
+		return (str(row[id_index]), {self.headers[i]: cell for i, cell in enumerate(row)})
+	
+	def outputRowsFromFile(self):
+		yield from [self.buildRowTuple(row) for row in self.getRowData()]
+	
+	
+	def outputRowsFromShelf(self):
+		yield from [(k,v) for item in self.shelf]
+
+
+class Merge(Stream):
+	def __init__(self, f, merge_type, outputFilePath):
+		super().__init__(f)
+		if merge_type == 'remove_page_duplicates':
+			self.resolve = self.pageDuplicatesResolve
+			self.transform = self.pageDuplicatesTransform
+			self.id_column = 'ID'
+		elif merge_type == 'merge_letter_pages':
+			self.resolve = self.mergeLetterPagesResolve
+			self.transform = self.mergeLetterPagesTransform
+			self.id_column = 'Letter'
 		
+		if os.path.isfile(outputFilePath):
+			os.remove(outputFilePath)
+
+		self.shelf_file = outputFilePath
+
+
+	def merge(self):
+		with shelve.open(self.shelf_file) as new_shelf:
+			for index, fields in self.stream():
+
+				print(type(self.resolve))
+				# Set up a clash of ids...
+
+				if index in new_shelf:
+					print('index in new shelf')
+					new_shelf[index] = self.resolve(new_shelf[index], fields)
+				else:
+					print('index not in new shelf')
+					new_shelf[index] = self.transform(fields)
+		
+			for k, v in new_shelf.items():
+				print(k, v)
+
+	def pageDuplicatesResolve(self, old, new):
+		if old['DT'] > new['DT']:
+			return old
+		elif new['DT'] > old['DT']:
+			return new
+
+	def pageDuplicatesTransform(self, field):
+		return field
+
+	def mergeLetterPagesResolve(self, old, new):
+		letter_key = a
+
+	def mergeLetterPagesTransform(self, field):
+		return field
+
+if __name__ == "__main__":
+	merge = Merge('spreadsheets/test_datetime.xlsx', 'remove_page_duplicates', 'output/datemerge.shelve')
+	
+	merge.merge()
+
+	
 
--- test/test_extractor/test_extractor.py
+++ test/test_extractor/test_extractor.py
@@ -2,13 +2,13 @@ import pytest
 
 import os
 
-from Extractor.Extractor import Extractor
+from Extractor.Extractor import Stream
 
 class TestExtractor:
 
 	def setup(self):
 		self.test_sheets_dir = "test/test_extractor/test_sheets/"
-		self.extractors = {name: Extractor(f_name) 
+		self.extractors = {name: Stream(f_name) 
 							for name, f_name in self.get_test_sheets().items()}
 
 
@@ -31,8 +31,14 @@ class TestExtractor:
 		assert list(self.extractors["test_simple"].getRowData()) == [['a_value1', 'b_value1', 1],
 					    									['a_value2', 'b_value2', 2]]
 
-	def test_buildRowDict(self):
+	def test_buildRowTuple(self):
 		self.extractors["test_simple"].id_column = "ID"
 		row = ['a_value1', 'b_value1', 1]
-		assert self.extractors["test_simple"].buildRowDict(row) == { '1' : { 'A_HEADER' : 'a_value1', 'B_HEADER': 'b_value1', 'ID': 1 }}
-
+		assert self.extractors["test_simple"].buildRowTuple(row) ==  ('1', { 'A_HEADER' : 'a_value1', 'B_HEADER': 'b_value1', 'ID': 1 } )
+	
+	def test_outputRows(self):
+		self.extractors["test_simple"].id_column = "ID"
+		print(list(self.extractors["test_simple"].outputRows()))
+		assert list(self.extractors["test_simple"].outputRows()) == [('1.0', { 'A_HEADER' : 'a_value1', 'B_HEADER': 'b_value1', 'ID': 1 } ),
+																		('2.0', { 'A_HEADER' : 'a_value2', 'B_HEADER': 'b_value2', 'ID': 2 } )]
+	
