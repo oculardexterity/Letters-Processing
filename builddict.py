0c54e057d7e751e5e60921a17645542c3e5d7933
Richard Hadden <oculardexterity@gmail.com>
2016-03-31 13:56:56 +0100

    cleared odd files

--- builddict.py
+++
@@ -1,7 +0,0 @@
-import shelve
-import os
-
-
-with shelve.open('revision_dict.shelve') as r:
-	for fname in [str(f)[:-4] for f in os.listdir('Letters 2015-12-08 0')]:
-		r[fname] = {'revision': 'FLAG: abstract needs to be proofed!', 'when': '2015-12-10T15:40:20', 'who': '#RB'}
